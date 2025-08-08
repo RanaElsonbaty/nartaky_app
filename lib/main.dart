95ed49ab9b28d84275cb790b6e856ff9f4fbf180
Rana Elsonbaty <ra01210772310@gmail.com>
2025-08-08 20:41:36 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nrtaky/core/helper_functions/global_storage.dart';
 import 'package:nrtaky/core/helper_functions/shared_preferences.dart';
 import 'package:nrtaky/core/routing/routes.dart';
+import 'package:nrtaky/firebase_options.dart';
 import 'package:nrtaky/nrtaky_app.dart';
 
 void main() async{
@@ -10,6 +12,9 @@ void main() async{
   await CacheHelper.appInitialization();
   await GlobalStorage.loadData();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
       EasyLocalization(
           supportedLocales: const [Locale('ar'), Locale('en')],
